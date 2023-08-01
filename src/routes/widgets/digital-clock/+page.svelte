b5ad8ab0f6038430fed43f5e4482e4a3806bbdda
Arczius <70692175+Arczius@users.noreply.github.com>
2023-08-01 13:20:13 +0200

    fixed the months not loading correctly

--- src/routes/widgets/digital-clock/+page.svelte
+++ src/routes/widgets/digital-clock/+page.svelte
@@ -42,18 +42,18 @@ const date_info = {
 }
 
 const monthMap = new Map([
-    [1, 'Januari'],
-    [2, 'February'],
-    [3, 'March'],
-    [4, 'April'],
-    [5, 'May'],
-    [6, 'June'],
-    [7, 'July'],
-    [8, 'August'],
-    [9, 'September'],
-    [10, 'October'],
-    [11, 'November'],
-    [12, 'December'],
+    [0, 'Januari'],
+    [1, 'February'],
+    [2, 'March'],
+    [3, 'April'],
+    [4, 'May'],
+    [5, 'June'],
+    [6, 'July'],
+    [7, 'August'],
+    [8, 'September'],
+    [9, 'October'],
+    [10, 'November'],
+    [11, 'December'],
 ])
 
 const dayMap = new Map([
